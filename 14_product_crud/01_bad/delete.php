78e5a060f5470f1795eb22b2b90df2271251a364
hasnain <hasnainkhankqs@gmail.com>
2021-05-03 08:35:35 +0500

    implement delete with mysqli oop

--- 14_product_crud/01_bad/delete.php
+++ 14_product_crud/01_bad/delete.php
@@ -1,16 +1,53 @@
 <?php
+// ----------- TODO delete image from directory -------------
 
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+// $id = $_POST['id'] ?? null;
+// if (!$id) {
+//     header('Location: index.php');
+//     exit;
+// }
+
+// $statement = $pdo->prepare('DELETE FROM products WHERE id = :id');
+// $statement->bindValue(':id', $id);
+// $statement->execute();
+
+$conn = new mysqli("localhost", "root", "", "products_crud");
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
 $id = $_POST['id'] ?? null;
 if (!$id) {
     header('Location: index.php');
     exit;
 }
 
-$statement = $pdo->prepare('DELETE FROM products WHERE id = :id');
-$statement->bindValue(':id', $id);
-$statement->execute();
+// $sql = "SELECT * FROM products where id = $id";
+// $result = mysqli_query($conn, $sql);
+// // Fetch all
+// $product = mysqli_fetch_row($result);
+// var_dump($product) ;
 
-header('Location: index.php');
+// $imagePath = 'images/'. $product['image'];
+// if(file_exists($imagePath)){
+
+//     //delete the image
+//     unlink($imagePath);
+// }
+
+$sql = "DELETE FROM products WHERE id=$id";
+$deleteResult = mysqli_query($conn, $sql);
+if ($deleteResult) {
+    header('Location: index.php');
+} else {
+    echo "some error occured";
+}
+
+// Free result set
+mysqli_free_result($deleteResult);
+// close connection
+mysqli_close($conn);
