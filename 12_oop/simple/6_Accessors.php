9304a638f9b0930594842175b09721bbb080e1cf
hasnain <teachingtechzone@gmail.com>
2021-06-11 15:19:10 +0500

    adding accessors

---
+++ 12_oop/simple/6_Accessors.php
@@ -0,0 +1,52 @@
+<?php
+
+// class Student {
+
+//     public $name;
+//     private $age;
+
+//     public function GetAge(){
+//         return $this -> age;
+//     }
+
+//     public function SetAge($a){
+//         $this -> age = $a;
+//     }
+
+// }
+
+
+// $std = new Student();
+// $std -> SetAge("Nehal");
+// echo $std -> GetAge();
+
+
+
+
+
+
+// class Student {
+//     private $email;
+
+//     public function GetEmail(){
+//         return $this -> email;
+//     }
+
+//     public function SetEmail($e){
+//         if(strpos($e,"@") > -1){
+//             $this -> email = $e;
+//         }
+//         else{
+//             echo "email not correct";
+//         }
+//     }
+
+// }
+
+
+// $std = new Student();
+// $std -> SetEmail("Nehalgmail.com");
+// echo $std -> GetEmail();
+
+
+?>
